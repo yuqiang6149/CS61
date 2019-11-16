65120fff92129d2e70aa59a39799239aed37a896
yuqiang <yuqiang6149@163.com>
2019-11-16 16:08:22 +0800

    nothing

--- CS61B/skeleton-sp18/lab1/LeapYear.java
+++ CS61B/skeleton-sp18/lab1/LeapYear.java
@@ -29,5 +29,17 @@ public class LeapYear {
             }
         }
     }
+    private static boolean isLeapYear(int year){
+   	if((year % 4 == 0 && year % 100 != 0) || year % 400 ==0)
+	{
+		return true;
+	}else{
+	
+	return false;}
+   
+   
+   
+    }
+
 }
 
